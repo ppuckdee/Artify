e409f52eee35c2f1a8c2b60f57e8dfe297596c8d
Nicholas Puckdee <132693105+ppuckdee@users.noreply.github.com>
2023-10-17 20:53:43 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,18 @@ document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
-app.append(header);
+app.appendChild(header);
+
+function createStyledCanvas() {
+  const canvas = document.createElement("canvas");
+  canvas.width = 256;
+  canvas.height = 256;
+
+  canvas.style.border = "1px solid black";
+  canvas.style.borderRadius = "10px";
+  canvas.style.boxShadow = "5px 5px 10px rgba(0, 0, 0, 0.5)";
+
+  app.appendChild(canvas);
+}
+
+createStyledCanvas();
